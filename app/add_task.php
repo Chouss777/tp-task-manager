d5d86eae57aa6f91ae3a0f5f1ffd4f09019549b6
Chouss777 <chabanioussama777@gmail.com>
2026-04-03 11:49:35 +0100

    Fix add_task.php - add insert logic

--- app/add_task.php
+++ app/add_task.php
@@ -1,1 +1,10 @@
-  
+<?php
+require 'db.php';
+
+if (!empty($_POST['title'])) {
+    $stmt = $pdo->prepare("INSERT INTO tasks (title) VALUES (?)");
+    $stmt->execute([$_POST['title']]);
+}
+
+header("Location: index.php");
+exit;
